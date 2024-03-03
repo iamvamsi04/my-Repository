81a6d21171c89588efd0b75da114d453e9e9820c
iamvamsi04 <158823710+iamvamsi04@users.noreply.github.com>
2024-03-03 13:37:50 +0530

    Add files via upload

---
+++ Diffinherit.java
@@ -0,0 +1,21 @@
+class Parent {
+    protected float protectedvar=10;
+    private float privatevar;
+    public float publicvar=11;
+}
+
+class child extends Parent {
+
+    public void Dispaly() {
+        System.out.println("public variable is "+publicvar);
+        System.out.println("protected variable is "+protectedvar);
+    }
+}
+
+public class Diffinherit {
+    public static void main(String[] args) {
+        child obj1 = new child();
+        obj1.Dispaly();
+        //System.out.println("protected variable is "+privatevar); results in error
+    }
+}
