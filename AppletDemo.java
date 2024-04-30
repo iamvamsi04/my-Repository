d74f462d1117bf6a9c2e8d6902e6a2e1cb96e527
iamvamsi04 <158823710+iamvamsi04@users.noreply.github.com>
2024-04-30 09:29:24 +0530

    Add files via upload

---
+++ AppletDemo.java
@@ -0,0 +1,15 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class AppletDemo extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello World", 100, 100);
+    }
+}
+
+/*
+ <html>
+ <applet code="AppletDemo.class" CodeBase="" ,width=100 height=100>
+ </applet>
+ </html>
+ */
